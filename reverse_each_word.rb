b6f55e36b628ff4ca00ace78d9f6cc7f7b36e2bc
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-26 00:04:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
-  original_string = string.split
+  original_string = string.split(" ")
   new_string = []
   original_string.each do |string|
      new_string << string.reverse
   end
-  new_string
+  new_string.join(" ")
 end
