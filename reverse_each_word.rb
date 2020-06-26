0022b4cbbfa38818794b073f86adc038a895693f
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-26 00:34:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -22,7 +22,7 @@ def reverse_each_word(array)
   og_array = array.split(" ")
   new_array = []
   og_array.collect do |array|
-  new_array << array.split
+  new_array << array.reverse
   end
   new_array.join(" ")
 end  
